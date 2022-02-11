89d19ebc857b8e8e0d5635582c7a35934370df36
azimsurani <azimsurani786@gmail.com>
2022-02-10 22:58:57 -0500

    Intermediate Files will be deleted after exit

--- src/assignment/Driver.java
+++ src/assignment/Driver.java
@@ -38,7 +38,7 @@ public class Driver {
 				displayFile();
 				break;
 			case 3:
-				twoPhaseMultiwayMergeSort();
+				TPMMS.twoPhaseMultiwayMergeSort(n);
 				break;
 			default:
 				deleteIntermediateFiles();
@@ -53,6 +53,8 @@ public class Driver {
 	}
 
 	private static void deleteIntermediateFiles() {
+		
+		
 
 
 		int memorySize = 2 * 1024; // Default size set to 2 MB
@@ -62,219 +64,18 @@ public class Driver {
 		int numberOfBlocks = n * integerSize / memorySize + ((n * integerSize / memorySize == 0) ? 0 : 1);
 
 		File file;
-		
+
 		for(int i=1;i<numberOfBlocks;i++) {
 			file = new File("sorted-"+i+".txt");
 			file.delete();
 			file = new File("combined-"+(i+1)+".txt");
 			file.delete();
 		}
-		
+
 		file = new File("sorted-"+numberOfBlocks+".txt");
 		file.delete();
-	}
-
-	private static void twoPhaseMultiwayMergeSort() {
-
-		if(n==0) {
-			System.out.println("Please generate the file first.");
-			return;
-		}
-
-		phase1();
-		phase2();
-
-	}
-
-	private static void phase1() {
-
-		int memorySize = 2 * 1024; // Default size set to 2 MB
-
-		int integerSize = 4; // Size of Integer in JAVA -- 4 Bytes
-
-		int numberOfBlocks = n * integerSize / memorySize + ((n * integerSize / memorySize == 0) ? 0 : 1);
-
-		int numberOfIntegers = memorySize/integerSize;
-
-		try {
-
-			Scanner fr = new Scanner(new FileInputStream("input.txt"));
-
-			System.out.println("\n----------Phase1---------\n");
-
-			int count=0;
-
-
-			for(int i=1;i<numberOfBlocks;i++) {
-
-				System.out.println("Proccesing Block : "+i);
-
-				int arr[] = new int[numberOfIntegers];
-
-				for(int j=0;j<arr.length;j++) {
-					arr[j] = fr.nextInt();
-					count++;
-				}
-
-				MergeSort.mergeSort(arr, 0, arr.length-1);
-
-				PrintWriter pw = new PrintWriter(new FileOutputStream("sorted-"+i+".txt"));
-
-				for(int j=0;j<arr.length;j++) {
-					pw.println(arr[j]);
-				}
-
-				pw.close();		
-				
-				System.out.println("sorted-"+i+".txt was created.\n");
-
-			}
-
-			System.out.println("Proccesing Block : "+numberOfBlocks);
-
-			int arr[] = new int[n-count];
-
-			for(int j=0;j<arr.length;j++) {
-				arr[j] = fr.nextInt();
-				count++;
-			}
-
-			MergeSort.mergeSort(arr, 0, arr.length-1);
-
-			PrintWriter pw = new PrintWriter(new FileOutputStream("sorted-"+numberOfBlocks+".txt"));
-
-			for(int j=0;j<arr.length;j++) {
-				pw.println(arr[j]);
-			}
-
-			pw.close();
-
-			fr.close();
-			
-			System.out.println("sorted-"+numberOfBlocks+".txt was created.\n");
-
-		} catch (FileNotFoundException e) {
-			System.out.println("Input file not found!");
-		}
-
-		System.out.println("\nPhase 1 was Completed Successfully");
-
-	}
-
-	private static void phase2() {
-
-		int memorySize = 2 * 1024; // Default size set to 2 MB
-
-		int integerSize = 4; // Size of Integer in JAVA -- 4 Bytes
-
-		int numberOfBlocks = n * integerSize / memorySize + ((n * integerSize / memorySize == 0) ? 0 : 1);
-
-		int numberOfIntegers = memorySize/integerSize;
-
-		for(int i=1;i<numberOfBlocks;i++) {
-
-			String file1="combined-"+(i)+".txt";
-			String file2="sorted-"+(i+1)+".txt";
-			String targetFile="combined-"+(i+1)+".txt";
-
-			if(i==1) {
-				file1= "sorted-"+(i)+".txt";	
-			}
-
-			if(i==numberOfBlocks-1) {
-				targetFile = "output.txt";
-			}
-
-			combineTwoFiles(file1, file2, targetFile, numberOfIntegers);
-
-		}
-
-	}
-
-
-	private static void combineTwoFiles(String file1,String file2,String targetFile,int numberOfIntegers) {
-
-
-		Queue queue1 = new Queue(numberOfIntegers/2);
-		Queue queue2 = new Queue(numberOfIntegers/2);
-
-		try {
-
-			Scanner fs1 = new Scanner(new FileInputStream(file1));
-			Scanner fs2 = new Scanner(new FileInputStream(file2));
-
-			PrintWriter pw = new PrintWriter(new FileOutputStream(targetFile));
-
-			while(!queue1.isFull() && fs1.hasNextInt()) {
-				queue1.enqueue(fs1.nextInt());
-			}
-
-			while(!queue2.isFull() && fs2.hasNextInt()) {
-				queue2.enqueue(fs2.nextInt());
-			}
-
-			int flag=0;
-
-			while(true) {
-
-				if(flag==1) {
-
-					if(fs1.hasNextInt()) {
-						queue1.enqueue(fs1.nextInt());
-					}
-					else if(queue1.isEmpty()) {
-
-						while(!queue2.isEmpty()) {
-							pw.println(queue2.dequeue());
-						}
-
-						while(fs2.hasNextInt()) {
-							pw.println(fs2.nextInt());
-						}
-
-						break;
-					}
-				}
-
-				if(flag==2) {
-
-					if(fs2.hasNextInt()) {
-						queue2.enqueue(fs2.nextInt());
-					}
-
-					else if(queue2.isEmpty()) {
-
-						while(!queue1.isEmpty()) {
-							pw.println(queue1.dequeue());
-						}
-
-						while(fs1.hasNextInt()) {
-							pw.println(fs1.nextInt());
-						}
-
-						break;
-					}
-				}
-
-
-				if(queue1.front()<queue2.front()) {
-					pw.println(queue1.dequeue());
-					flag=1;
-				}else {
-					pw.println(queue2.dequeue());
-					flag=2;
-				}
-			}
-
-			fs1.close();
-			fs2.close();
-			pw.close();
-
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		System.out.println("All the intermediate files were deleted.");
 
 	}
 
@@ -287,7 +88,6 @@ public class Driver {
 		}
 
 		try {
-
 
 			Scanner fr = new Scanner(new FileInputStream("input.txt"));
 
@@ -337,11 +137,10 @@ public class Driver {
 
 			pw.close();
 
-			System.out.println("\n----File was created successfully-----\n");
+			System.out.println("\n----input.txt was created successfully-----\n");
 
 
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 			System.out.println("Please try again");
 		}
 
